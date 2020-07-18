dca26688076e14d5ae834164915d05ada77f8394
luis cespedes <lxluxo23@gmail.com>
2020-07-17 20:35:35 -0400

    prueba editando html

--- resources/views/leanding/2.blade.php
+++ resources/views/leanding/2.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="py-5 text-center">
       <img class="d-block mx-auto mb-4" src="../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-      <h2>Checkout form</h2>
-      <p class="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+      <h2>pago</h2>
+      <p class="lead">awa de uwu</p>
     </div>
   
     <div class="row">
