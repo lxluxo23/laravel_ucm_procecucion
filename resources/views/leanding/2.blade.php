a17c96a6797d92776dc7f6a1964e2ce8d0251be9
Jose Ibarra <joseiic971@gmail.com>
2020-07-17 20:40:36 -0400

    cambio en el mismo archivo

--- resources/views/leanding/2.blade.php
+++ resources/views/leanding/2.blade.php
@@ -11,21 +11,21 @@
     <div class="row">
       <div class="col-md-4 order-md-2 mb-4">
         <h4 class="d-flex justify-content-between align-items-center mb-3">
-          <span class="text-muted">Your cart</span>
+          <span class="text-muted">Tu Tarjeta</span>
           <span class="badge badge-secondary badge-pill">3</span>
         </h4>
         <ul class="list-group mb-3">
           <li class="list-group-item d-flex justify-content-between lh-condensed">
             <div>
-              <h6 class="my-0">Product name</h6>
-              <small class="text-muted">Brief description</small>
+              <h6 class="my-0">Nombre del producto</h6>
+              <small class="text-muted">description</small>
             </div>
-            <span class="text-muted">$12</span>
+            <span class="text-muted">$100</span>
           </li>
           <li class="list-group-item d-flex justify-content-between lh-condensed">
             <div>
-              <h6 class="my-0">Second product</h6>
-              <small class="text-muted">Brief description</small>
+              <h6 class="my-0">Otro producto</h6>
+              <small class="text-muted">description</small>
             </div>
             <span class="text-muted">$8</span>
           </li>
@@ -59,7 +59,7 @@
         </form>
       </div>
       <div class="col-md-8 order-md-1">
-        <h4 class="mb-3">Billing address</h4>
+        <h4 class="mb-3">DATOS De compra</h4>
         <form class="needs-validation" novalidate>
           <div class="row">
             <div class="col-md-6 mb-3">
