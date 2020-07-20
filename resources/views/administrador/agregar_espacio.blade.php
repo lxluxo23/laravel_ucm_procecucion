d596dc43cb53d693ef65d376729d14570c8d2d65
luis cespedes <lxluxo23@gmail.com>
2020-07-20 17:05:21 -0400

    Update agregar_espacio.blade.php

--- resources/views/administrador/agregar_espacio.blade.php
+++ resources/views/administrador/agregar_espacio.blade.php
@@ -7,14 +7,7 @@
     <div class="col-md-12 order-md-1">
       <h4 class="mb-3">Agregar Espacio de trabajo</h4>
       <form class="needs-validation" novalidate>
-        <div class="row">
-          <div class="col-md-6 mb-6">
-            <label for="rut">Nombre</label>
-            <input type="text" class="form-control" id="nombre" placeholder="" value="" required>
-            <div class="invalid-feedback">
-              Ingrese Nombre por favor
-            </div>
-          </div>
+        <div class="row">         
           <div class="col-md-6 mb-3">
             <label for="nombre">ID</label>
             <input type="text" class="form-control" id="ID" placeholder="" value="" required>
@@ -22,20 +15,18 @@
               Ingrese ID
             </div>
           </div>
+          <div class="col-md-6 mb-6">
+            <label for="Capacidad">Capacidad</label>
+            <input type="number" class="form-control" id="capacidad" placeholder="" required>
+            <div class="invalid-feedback">
+              Ingrese Cantidad
+            </div>
         </div>
 
-        <div class="mb-3">
-          <label for="email">E-mail <span class="text-muted"></span></label>
-          <input type="email" class="form-control" id="email" placeholder="">
-          <div class="invalid-feedback">
-            Porfavor ingrese un E-mail valido.
+          <div class="col-md-6 mb-4">
+            <label for="descripcion">Descripcion<span class="text-muted"></span></label>
+            <textarea class="form-control" id="descripcion" rows="5"></textarea>
           </div>
-        </div>
-
-        <div class="mb-3">
-          <label for="telefono">Numero de Telefono</label>
-          <input type="number" class="form-control" id="telefono" placeholder="" required>
-        </div>
         
         <hr class="mb-4">
         <button class="btn btn-primary btn-lg btn-block" type="submit">Guardar</button>
