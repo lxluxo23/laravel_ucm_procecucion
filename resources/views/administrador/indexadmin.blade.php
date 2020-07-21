9a2fade86f1bc57863bb955899a998cb51d8d34f
felipe herrera <pipeherreguz@gmail.com>
2020-07-20 20:02:31 -0400

    ASD2

--- resources/views/administrador/indexadmin.blade.php
+++ resources/views/administrador/indexadmin.blade.php
@@ -20,7 +20,7 @@
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Ingreso mensual</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">$RESIVIR DATO DE BD</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">$RECIBIR DATO DE BD</div>
                       </div>
                       <div class="col-auto">
                         <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -37,7 +37,7 @@
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-success text-uppercase mb-1">iNGRESO ANUAL</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">$RESIVIR DATO DE BD</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">$RECIBIR DATO DE BD</div>
                       </div>
                       <div class="col-auto">
                         <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -56,7 +56,7 @@
                         <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Capasidad</div>
                         <div class="row no-gutters align-items-center">
                           <div class="col-auto">
-                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">RESIVIR DATO DE BD%</div>
+                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">RECIBIR DATO DE BD%</div>
                           </div>
                           <div class="col">
                             <div class="progress progress-sm mr-2">
@@ -80,7 +80,7 @@
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pendientes</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">RESIVIR DATO DE BD</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">RECIBIR DATO DE BD</div>
                       </div>
                       <div class="col-auto">
                         <i class="fas fa-comments fa-2x text-gray-300"></i>
