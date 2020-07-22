997d9c9a173a5052402642fdd3e5557d59a72f13
luis cespedes <lxluxo23@gmail.com>
2020-07-22 18:34:46 -0400

    Update listar_espacio.blade.php

--- resources/views/administrador/listar_espacio.blade.php
+++ resources/views/administrador/listar_espacio.blade.php
@@ -35,7 +35,7 @@
                 <td>{{$item->Descripcion}}</td>
                 <td>{{$item->Estado}}</td>
                 <td>{{$item->precio}}</td>
-                <td>{{$item->url_img}}</td>
+                <td><IMG SRC="images/{{$item->url_img}}"></td>
                 <td>LOS BOTONES</td>
               </tr>
               @endforeach
