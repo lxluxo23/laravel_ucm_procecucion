35fa43b91a0c65e188604c5c791fdb25e4289eeb
felipe herrera <pipeherreguz@gmail.com>
2020-07-23 00:49:59 -0400

    cambioinicio6

--- resources/views/administrador/listar_espacio.blade.php
+++ resources/views/administrador/listar_espacio.blade.php
@@ -5,7 +5,7 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-2 text-gray-800">Espacios</h1>
+    <h1 class="h3 mb-2 text-gray-800">Espacios de trabajo</h1>
     <p class="mb-4">Modifique o elimine seleccionando los botones a la derecha en la fila correspondiente al espacio de trabajo.</p>
 
     <!-- DataTales Example -->
@@ -35,7 +35,7 @@
                 <td>{{$item->Descripcion}}</td>
                 <td>{{$item->Estado}}</td>
                 <td>{{$item->precio}}</td>
-                <td><IMG SRC="images/{{$item->url_img}}" width="150"></td>
+                <td><IMG SRC="images/{{$item->url_img}}" width="100" height="100"></td>
                 <td><a href='delete.php?id=".$row["0"]."'><img id='img_tab_edit' src='images/edit.jpg' width="50"/></a> 
                     <a href='delete.php?id=".$row["0"]."'><img id='img_tab_delete' src='images/elim.jpg' width="50"/></a>
                 </td>
