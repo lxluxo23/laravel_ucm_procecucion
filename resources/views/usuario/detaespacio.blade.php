28ee824bf9b9704ec037c977007747c7c37754d8
felipe herrera <pipeherreguz@gmail.com>
2020-07-24 17:00:23 -0400

    cambiodetespacio

--- resources/views/usuario/detaespacio.blade.php
+++ resources/views/usuario/detaespacio.blade.php
@@ -23,27 +23,28 @@
                             <div class="row">    
                                 
                                 <div class="col-md-6">
-                                    <img src="images/espacio1.jpg" alt="">
+                                    <img src="images/espacio1.jpg" alt="" class="imagenDeTVenta">
                                 </div>
                                 
-                                <div class="col-md-6">
-                                    <label for="Capacidad">Capacidad</label>
-                                    <label class="form-control" id="capacidad" name="capacidad">Este espacio tiene una capacidad hasta para 100 personas</label>
-                                </div>
-
                                 <div class="col-md-6">
                                     <label for="descripcion">Descripcion</label>
                                     <textarea class="form-control" id="descripcion" name="descripcion" rows="5">Ideal para trabajar junto a un grupo decente de personas, el cual trae una cocina y un baño en una misma habitacion, uno al lado de otro.
                                     </textarea>
                                 </div>
 
+
+                                <div class="col-md-6">
+                                    <label for="Capacidad">Capacidad</label>
+                                    <label class="form-control" id="capacidad" name="capacidad">Este espacio tiene una capacidad hasta para 100 personas</label>
+                                </div>
+
                                 <div class="col-md-6">
                                     <label for="precio">Precio</label>
                                     <label class="form-control" id="precio" name="precio">200 pesitos x dia</label>
                                 </div>
-                        
+                
+                                <div class="col-md-6"></div>
 
-                        
                                 <div class="col-md-6">
                                     <button class="btn btn-success btn-lg btn-block" type="submit">Arrendar espacio</button>
                                 </div>
