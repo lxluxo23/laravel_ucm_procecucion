e8ad2fe8101d46bec1145a4c7c5f84c9db14523e
felipe herrera <pipeherreguz@gmail.com>
2020-07-28 17:45:09 -0400

    ordenycierrededivs_agregar_espacio

--- resources/views/administrador/agregar_espacio.blade.php
+++ resources/views/administrador/agregar_espacio.blade.php
@@ -3,33 +3,34 @@
 @section('contenidoadmin')
 <div class="container-fluid">
   <div class="row">
-    
-      <div class="col-md-12 order-md-1">
+    <div class="col-md-12 order-md-1">
 
         @if(session('mensaje'))
 
-        <div class="alert alert-success" onclick="window.close()"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-          <strong>!</strong>
+          <div class="alert alert-success" onclick="window.close()"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+            <strong>!</strong>
             {{ session('mensaje') }}
-        </div>
+          </div>
 
         @endif
         <h4 class="mb-3">Agregar Espacio de trabajo</h4>
         <form action ="{{ route('crear_espacio') }}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data">
           @csrf
+
           <div class="row">         
-            <div class="col-md-6 mb-6">
+            <div class="col-md-6 mb-4">
               <label for="Capacidad">Capacidad</label>
               <input type="number" class="form-control" id="capacidad" name="capacidad" placeholder="" required>
               <div class="invalid-feedback">
                 Ingrese Cantidad
               </div>
             </div>
-            <div class="col-md-6 mb-3">
-            <label for="precio">Precio</label>
-              <input type="number" class="form-control" id="precio" name="precio" placeholder="" value="" required>
+
+            <div class="col-md-6 mb-6">
+              <label for="categoria">Categoria</label>
+              <input type="number" class="form-control" id="categoria" name="categoria" placeholder="" required>
               <div class="invalid-feedback">
-                Ingrese precio
+                Ingrese Categoria
               </div>
             </div>
 
@@ -38,13 +39,20 @@
               <textarea class="form-control" id="descripcion" name="descripcion" rows="5"></textarea>
             </div>
 
-            
-            <div class="col-md-6 mb-4">
-            <label class="col-md-4 control-label">Nuevo Archivo</label>
-                <div class="col-md-6">
-                  <input type="file" class="form-control" name="file" onchange="loadFile(event)" >
+            <div class="col-md-6 mb-6">
+              <label for="precio">Precio</label>
+              <input type="number" class="form-control" id="precio" name="precio" placeholder="" value="" required>
+              <div class="invalid-feedback">
+                Ingrese precio
+              </div>
+            </div>
+
   
-                  <img name="portada" id="portada" width="250"/>
+            <div class="col-md-6 mb-4">
+              <label class="col-md-4 control-label">Nuevo Archivo</label>
+              <div class="col-md-6">
+                <input type="file" class="form-control" name="file" onchange="loadFile(event)" >
+                <img name="portada" id="portada" width="250"/>
                   <script>
                       var loadFile = function(event) {
                       var reader = new FileReader();
@@ -55,13 +63,14 @@
                       reader.readAsDataURL(event.target.files[0]);
                       };
                   </script>
-                </div>
-          
-                <hr class="mb-4">
-                  <button class="btn btn-primary btn-lg btn-block" type="submit">Guardar</button>
+              </div> 
+              <hr class="mb-4">
+              <button class="btn btn-primary btn-lg btn-block" type="submit">Guardar</button>
             </div>
+
+          </div>
         </form>
-      </div>
+    </div>
   </div>
 </div>
     
