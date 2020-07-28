901e52f27ad99c0acf24ea55a606489624164d0e
Jose Ibarra <joseiic971@gmail.com>
2020-07-27 21:05:15 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', 'LandingController@index')->name('inicio');
+Route::get('/', 'LandingController@index')->name('inicio');
 
 Route::get('/pagogogogo', 'LandingController@pago')->name('pago');
 
