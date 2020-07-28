54244cf6c392601289b309087a3d4be5e41764af
felipe herrera <pipeherreguz@gmail.com>
2020-07-28 17:16:18 -0400

    Update modificarespacio.blade.php
    orden y arreglo de cierre de div

--- resources/views/administrador/modificarespacio.blade.php
+++ resources/views/administrador/modificarespacio.blade.php
@@ -19,72 +19,69 @@
     <form method="POST" action="{{route('actualizarespacio')}}" accept-charset="UTF-8" enctype="multipart/form-data">
         @csrf
         <div class="row">         
-        <div class="col-md-3 mb-3">
-          <label for="id_espacio">ID</label>
-        <input type="number" class="form-control" id="id_espacio" name="id_espacio" placeholder="" value="{{$item->ID_espacio_trabajo}}" required>
-          <div class="invalid-feedback">
+          <div class="col-md-3 mb-3">
+            <label for="id_espacio">ID</label>
+            <input type="number" class="form-control" id="id_espacio" name="id_espacio" placeholder="" value="{{$item->ID_espacio_trabajo}}" required>
+            <div class="invalid-feedback">
+            </div>
           </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <label for="capacidad">Capacidad</label>
-        <input type="number" class="form-control" id="capacidad" name="capacidad" placeholder="" value="{{$item->Capacidad}}" required>
-          <div class="invalid-feedback">
-            Ingrese Capacidad
+
+          <div class="col-md-3 mb-3">
+            <label for="capacidad">Capacidad</label>
+            <input type="number" class="form-control" id="capacidad" name="capacidad" placeholder="" value="{{$item->Capacidad}}" required>
+            <div class="invalid-feedback">
+              Ingrese Capacidad
+            </div>
           </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <label for="precio">Precio</label>
-        <input type="number" class="form-control" id="precio" name="precio" placeholder="" value="{{$item->precio}}" required>
-          <div class="invalid-feedback">
-            Ingrese Precio
+
+          <div class="col-md-3 mb-3">
+            <label for="precio">Precio</label>
+            <input type="number" class="form-control" id="precio" name="precio" placeholder="" value="{{$item->precio}}" required>
+            <div class="invalid-feedback">
+              Ingrese Precio
+            </div>
           </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <label for="Estado">Estado</label>
-          <select id="combobo" name="combobo" class="form-control">
-            
+
+          <div class="col-md-3 mb-3">
+            <label for="Estado">Estado</label>
+            <select id="combobo" name="combobo" class="form-control"> 
             @foreach ($los_datos_a_modificar as $item)
-            <option value="Disponible">Disponible</option>
-            <option value="No disponible">No disponible</option>
+              <option value="Disponible">Disponible</option>
+              <option value="No disponible">No disponible</option>
             @endforeach
-         
-           
-          </select>
+            </select>
+          </div>
+
+          <div class="col-md-12 mb-4">
+            <label for="descripcion">Descripcion<span class="text-muted"></span></label>
+            <textarea class="form-control" id="descripcion" name="descripcion" rows="5">{{$item->Descripcion}}</textarea>
+          </div>
+
+          <div class="col-md-6 mb-4">
+            <label class="col-md-4 control-label">Editar Foto</label>
+                <div class="col-md-6">
+                  <input type="file" class="form-control" name="file" onchange="loadFile(event)" >
+                  <img name="portada" id="portada" width="250"/>
+                  <script>
+                      var loadFile = function(event) {
+                      var reader = new FileReader();
+                      reader.onload = function(){
+                        var output = document.getElementById('portada');
+                        output.src = reader.result;
+                      };
+                      reader.readAsDataURL(event.target.files[0]);
+                      };
+                  </script>
+                </div>
+          </div>
+   
+          <hr class="mb-4">
+          <button class="btn btn-primary btn-lg btn-block" type="submit">Guardar</button>
         </div>
-        <div class="col-md-12 mb-4">
-          <label for="descripcion">Descripcion<span class="text-muted"></span></label>
-        <textarea class="form-control" id="descripcion" name="descripcion" rows="5">{{$item->Descripcion}}</textarea>
-        </div>
-
-        <div class="col-md-6 mb-4">
-          <label class="col-md-4 control-label">Editar Foto</label>
-              <div class="col-md-6">
-                <input type="file" class="form-control" name="file" onchange="loadFile(event)" >
-                <div>
-                </div>  
-                <img name="portada" id="portada" width="250"/>
-                <script>
-                    var loadFile = function(event) {
-                    var reader = new FileReader();
-                    reader.onload = function(){
-                      var output = document.getElementById('portada');
-                      output.src = reader.result;
-                    };
-                    reader.readAsDataURL(event.target.files[0]);
-                    };
-                </script>
-              </div>
-
-
-
-        </div>
-
-             
-        <hr class="mb-4">
-        <button class="btn btn-primary btn-lg btn-block" type="submit">Guardar</button>
-      </form>
+    </form>
     @endforeach
 
   </div>
+</div>
 </div>
 @endsection
