2fd5d8721fb19529fa45a5988353ceeb26e31620
felipe herrera <pipeherreguz@gmail.com>
2020-07-29 19:07:55 -0400

    Update detaespacio.blade.php

--- resources/views/usuario/detaespacio.blade.php
+++ resources/views/usuario/detaespacio.blade.php
@@ -21,7 +21,7 @@
                         @csrf
                         @foreach ($dato_espacio as $item)
                         <br>
-                        <h4 class="mb-3">Características de {{$item->nombre_cat}}</h4>
+                        <h4 class="mb-3" style="font-size: 1.5rem">Características de {{$item->nombre_cat}}</h4>
                         <br>
                             
                             <div class="row">    
@@ -32,15 +32,15 @@
                                     </div>
                                     
                                     <div class="col-md-6">
-                                        <label for="descripcion"><b>Descripcion</b></label>
-                                        <label class="form-control" style="border: 1px solid  #2ba7e044; height: 6.5em; font-size: 1.15rem; overflow: scroll" id="descripcion" name="descripcion" rows="5">{{$item->descripcion}}
+                                        <label for="descripcion" style="font-size: 1.1rem"><b>Descripcion</b></label>
+                                        <label class="form-control" style="border: 1px solid  #2ba7e044; height: 6.5em; font-size: 1.1rem; overflow: auto" id="descripcion" name="descripcion" rows="5">{{$item->descripcion}}
                                         </label>
                                         <br>
-                                        <label for="Capacidad"><b>Capacidad</b></label>
-                                        <label class="form-control" style="border: 1px solid  #2ba7e044; font-size: 1.15rem" id="capacidad" name="capacidad">Para {{$item->capacidad}} personas</label>
+                                        <label for="Capacidad" style="font-size: 1.1rem"><b>Capacidad</b></label>
+                                        <label class="form-control" style="border: 1px solid  #2ba7e044; font-size: 1.1rem" id="capacidad" name="capacidad">Para {{$item->capacidad}} personas</label>
                                         <br>
-                                        <label for="precio"><b>Precio</b></label>
-                                        <label class="form-control" style="border: 1px solid  #2ba7e044; font-size: 1.15rem" id="precio" name="precio">${{$item->precio}} x día</label>
+                                        <label for="precio" style="font-size: 1.1rem"><b>Precio</b></label>
+                                        <label class="form-control" style="border: 1px solid  #2ba7e044; font-size: 1.1rem" id="precio" name="precio">${{$item->precio}} x día</label>
 
                                     </div>
           
