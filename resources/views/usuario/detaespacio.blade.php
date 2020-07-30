194abaa5e7c45b39861528f9626a6563f812d590
felipe herrera <pipeherreguz@gmail.com>
2020-07-30 16:11:16 -0400

    pruebamodul_ventanadetespacio

--- resources/views/usuario/detaespacio.blade.php
+++ resources/views/usuario/detaespacio.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.landing')
 <link rel="stylesheet" href="../assets/landing/css/estilo.css">
+
+
 <div class="contenedordeinicio">
     <div class="fondoblancotransp">
     </div>
         <div class="FondoContenedorDetalleEspacio">
-
             <div class="container-fluid">
                 <div class="row">
                    
@@ -48,17 +49,69 @@
 
                                     <div class="col-md-6">
                                   
-                                        <button class="btn btn-success btn-lg btn-block" type="submit">Arrendar espacio</button>
+                                        <button class="btn btn-success btn-lg btn-block" type="submit" data-toggle="modal" data-target="#exampleModal">Arrendar espacio</button>
+                                        
+                                        <!-- Modal -->
+                                        
+                                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog modal-xl">
+                                              <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">Desea arrendar {{$item->nombre_cat}}?</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <label>Porfavor complete los datos a continuación</label>
+                                                    <br>
+                                                    <br>
+                                                    <!-- Formulario Modal -->
+                                                    <form>
+                                                        <div class="container-fluid">
+                                                            <div class="col-md-12 order-md-1">
+                                                                <div class="row">
+                                                                    <div class="col-md-6">
+                                                                        <img src="../images/{{$item->url_img}}" style="background-size: 20%" alt="" class="imagenDeTVenta">
+                                                                        {{$item->descripcion}}
+                                                                    </div>
+                                                              
+                                                                                                                                                                                                
+                                                                    <div class="col-md-6">
+                                                                        <div class="col-md-6">
+                                                                            <label for="fini">Elija el día que desea usar el servicio</label>                                                               
+                                                                            <input type="number" class="form-control" id="fini" name="fini" placeholder="" required>
+                                                                        </div>
+                                                                        <div class="col-md-6">
+                                                                            <label for="fini">Hasta el día (incluyéndose)</label>
+                                                                            <input type="number" class="form-control" id="fini" name="fini" placeholder="" required>
+                                                                        </div>
+                                                                 
+                                                                    </div>
+                                                                </div>
+
+                                                                
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                                <div class="modal-footer">
+                                                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                                  <button type="button" class="btn btn-primary">Hacer compra</button>
+                                                </div>
+                                              </div>
+                                            </div>
+                                        </div>
+
                                     </div>
                                 
                                 
                             </div>
                         @endforeach
-                        <!-- </form> -->
+                        
                     </div>
                     
                 </div>
             </div>
-
-  
+        </div>
 </div>
