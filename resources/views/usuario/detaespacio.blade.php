bdb06d2425cf18b9f677b1ad6b6f3abc5e4c10b2
felipe herrera <pipeherreguz@gmail.com>
2020-08-07 00:57:27 -0400

    Update detaespacio.blade.php

--- resources/views/usuario/detaespacio.blade.php
+++ resources/views/usuario/detaespacio.blade.php
@@ -127,7 +127,7 @@
         $("#fini").val(today);
   
         document.getElementById("fini").addEventListener("change",function(){
-            if (today>(document.getElementById('fini').value) || (document.getElementById('fini').value)>(document.getElementById('ffin').value)){
+            if (today>(document.getElementById('fini').value) || (document.getElementById('ffin').value) && (document.getElementById('fini').value)>(document.getElementById('ffin').value)){
                 alert("La fecha no puede ser menor a la de hoy o mayor a la fecha final")
                 $("#fini").val("");
             };
