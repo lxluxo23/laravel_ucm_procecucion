484b2e2469c60856495bfa2c693cdc5eea26c056
luis cespedes <lxluxo23@gmail.com>
2020-08-18 19:48:51 -0400

    Update landing.blade.php

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -62,7 +62,7 @@
             <!-- drop box -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <span class="mr-2 d-none d-lg-inline text-white small">{{session('email')}}</span>
+              <span class="mr-2 d-none d-lg-inline ">{{session('email')}}</span>
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
