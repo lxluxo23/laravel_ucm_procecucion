eea8d13766050b86d82c0d5a3be2f1d1181ab601
luis cespedes <lxluxo23@gmail.com>
2020-08-18 20:00:10 -0400

    detalle icono

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -62,7 +62,7 @@
             <!-- drop box -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <span class="mr-2 d-none d-lg-inline ">{{session('email')}}</span>
+              <span class="mr-2 d-none d-lg-inline">{{session('email')}} <i class="fas fa-angle-down"></i></span>
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
--- resources/views/layouts/plantillaadmin.blade.php
+++ resources/views/layouts/plantillaadmin.blade.php
@@ -181,7 +181,7 @@
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle"  href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span style="color:white" class="mr-2 d-none d-lg-inline ">{{session('email')}}</span>
+                <span style="color:white" class="mr-2 d-none d-lg-inline ">{{session('email')}} <i class="fas fa-angle-down"></i></span>
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">          
